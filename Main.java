55ab8794dd3855c86b72fd0c0a222ae6cc030da1
lisekxd1234 <lisekxd1234@gmail.com>
2024-04-07 12:11:54 +0200

    Dodano plik Main.java oraz katalog geometry

--- Main.java
+++ Main.java
@@ -1,20 +1,18 @@
+import geometry.Circle;
 import geometry.ColoredCircle;
 import geometry.Point;
 
 public class Main {
     public static void main(String[] args) {
-        // Tworzenie obiektu klasy Point reprezentującego środek koła
-        Point center = new Point(0, 0);
+        // Tworzenie tablicy obiektów typu Circle i ColoredCircle
+        Circle[] circles = new Circle[2];
+        circles[0] = new Circle(new Point(0, 0), 5);
+        circles[1] = new ColoredCircle(new Point(1, 1), 3, "red");
         
-        // Tworzenie obiektu klasy ColoredCircle z podanym środkiem, promieniem i kolorem
-        ColoredCircle coloredCircle = new ColoredCircle(center, 5, "blue");
-        
-        // Wyświetlenie właściwości kolorowego koła
-        System.out.println("Właściwości kolorowego koła:");
-        System.out.println("Środek koła: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
-        System.out.println("Promień koła: " + coloredCircle.getRadius());
-        System.out.println("Kolor koła: " + coloredCircle.getColor());
-        System.out.println("Obwód koła: " + coloredCircle.calculatePerimeter());
-        System.out.println("Pole powierzchni koła: " + coloredCircle.getArea());
+        // Iteracja przez tablicę i wywołanie metody getArea() dla każdego obiektu
+        for (Circle circle : circles) {
+            double area = circle.getArea();
+            System.out.println("Pole powierzchni koła: " + area);
+        }
     }
 }
